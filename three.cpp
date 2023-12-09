d54c7f38d8cdcada5db6bb7730c7b364fa8b3edc
Miguel Perez <perezoji@cs.fsu.edu>
2023-12-08 21:34:35 -0800

    three is worling now

--- three.cpp
+++ three.cpp
@@ -1,8 +1,6 @@
 #include <cctype>
 #include <iostream>
 #include <vector>
-#include <ranges>
-#include <format>
 #include <iterator>
 
 using namespace std;
@@ -15,8 +13,6 @@ auto is_touching(const vector<string> & schematic, int x, int y) -> bool {
     int down = y + 1 > schematic[0].size() - 1 ? schematic[0].size() - 1 : y + 1;
 
     auto check = [&](int xx, int yy) { return !isalnum(schematic[xx][yy]) && schematic[xx][yy] != '.'; };
-
-    // cout << format("({}, {}) left: {} right: {} up: {} down: {}\n", x, y, left, right, up, down);
     
     return check(left, up) || check (left, y) || 
            check(left, down) || check(x, up) || 
@@ -34,7 +30,7 @@ int main() {
 
     for (const auto &line: schematic) {
         int char_index = 0;
-        
+
         for (const auto &c: line) {
             if (isdigit(c)) {
                 current_part_number += c; 
@@ -45,7 +41,6 @@ int main() {
 
             if (c == '.' || !isalnum(c)) {
                 if (touching_symbol) {
-                    cout << format("Part number: {}\n", current_part_number);
                     running_total += stoi(current_part_number);
                     touching_symbol = false;
                 }   
@@ -54,6 +49,12 @@ int main() {
             }
 
             char_index++;
+
+            if (char_index == line.size() && touching_symbol) {
+                running_total += stoi(current_part_number);
+                touching_symbol = false;
+                current_part_number = "";
+            }
         }
 
         line_index++;
